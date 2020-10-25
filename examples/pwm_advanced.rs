79c2f3c4c13ca451e3fae62c25ffecb6c8c37d0a
Morgan LaMoore <morganl@gmail.com>
2020-10-25 15:41:36 -0500

    Updated pwm_advanced example to use TIM1
    Preparing for testing complementary PWM and advanced timer features

--- examples/pwm_advanced.rs
+++ examples/pwm_advanced.rs
@@ -28,22 +28,21 @@ fn main() -> ! {
 
     // Acquire the GPIOA and B peripheral. This also enables the clock for
     // GPIOA and B in the RCC register.
-    let gpioa = dp.GPIOA.split(ccdr.peripheral.GPIOA);
+    let _gpioa = dp.GPIOA.split(ccdr.peripheral.GPIOA);
     let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
+    let gpioe = dp.GPIOE.split(ccdr.peripheral.GPIOE);
 
     // Select PWM output pins
     let tim3_pins = gpiob.pb0.into_alternate_af2();
-    let _tim1_pins = gpioa.pa8.into_alternate_af1();
-    let _tim1_npins = gpiob.pb14.into_alternate_af1();
+    let tim1_pins = (gpioe.pe13.into_alternate_af1(), gpioe.pe11.into_alternate_af1(), gpioe.pe9.into_alternate_af1());
+    let _tim1_npins = (gpioe.pe12.into_alternate_af1(), gpioe.pe10.into_alternate_af1(), gpioe.pe8.into_alternate_af1());
 
     info!("");
     info!("stm32h7xx-hal example - PWM");
     info!("");
 
     // Configure PWM at 10kHz
-    let mut tim3_pwm =
-        dp.TIM3
-            .pwm(tim3_pins, 1.hz(), ccdr.peripheral.TIM3, &ccdr.clocks);
+    let mut tim3_pwm = dp.TIM3.pwm(tim3_pins, 1.hz(), ccdr.peripheral.TIM3, &ccdr.clocks);
 
     // Output PWM on PA8
     let max = tim3_pwm.get_max_duty();
@@ -51,6 +50,17 @@ fn main() -> ! {
 
     info!("25%");
     tim3_pwm.enable();
+
+    let (mut t1c1, mut t1c2, mut t1c3) = dp.TIM1.pwm(tim1_pins, 2.hz(), ccdr.peripheral.TIM1, &ccdr.clocks);
+
+    let max = t1c1.get_max_duty();
+    t1c1.set_duty( max / 4 * 1);
+    t1c2.set_duty( max / 4 * 2);
+    t1c3.set_duty( max / 4 * 3);
+    
+    t1c1.enable();
+    t1c2.enable();
+    t1c3.enable();
 
     loop {}
 }
